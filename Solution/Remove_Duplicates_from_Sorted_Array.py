0cf7bc904439e149cdffe97453fbee4cccb1829d
shinPark43 <141878853+shinPark43@users.noreply.github.com>
2025-01-22 11:36:49 -0600

    Found better solution

--- Solution/Remove_Duplicates_from_Sorted_Array.py
+++ Solution/Remove_Duplicates_from_Sorted_Array.py
@@ -4,19 +4,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        stack = []
-        curr = 0
-        for i in range(len(nums)):
-            if curr == 0:
-                stack.append(nums[curr])
-                isFirst = False
-                curr += 1
-            elif nums[curr] != stack[-1]:
-                stack.append(nums[curr])
-                curr += 1
-            elif nums[curr] == stack[-1]:
-                nums.pop(curr)
-                nums.append('_')
-        return curr
+        unique_index = 0
+
+        for i in range(1, len(nums)):
+            if (nums[unique_index] != nums[i]):
+                unique_index += 1
+                nums[unique_index] = nums[i]
+        return unique_index + 1
 
         
