cc3d1386fd5dae0083bfe618dfbe98fc114eed1a
shinPark43 <141878853+shinPark43@users.noreply.github.com>
2025-01-30 23:57:18 -0600

    Solved one array problem

---
+++ Solutions/Array/Find_Words_Containing_Character.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findWordsContaining(self, words, x):
+        """
+        :type words: List[str]
+        :type x: str
+        :rtype: List[int]
+        """
+        return [i for i in range(0, len(words)) if x in words[i]] 
