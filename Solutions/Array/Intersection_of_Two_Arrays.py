6dfe7a4e40dd7ce073d2520c6609316aa41e380e
Shinhyuk Park <141878853+shinPark43@users.noreply.github.com>
2025-05-07 00:20:59 -0500

    Solved one array problem

---
+++ Solutions/Array/Intersection_of_Two_Arrays.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+
+        num_intersection = nums1.intersection(nums2)
+        return list(num_intersection)
