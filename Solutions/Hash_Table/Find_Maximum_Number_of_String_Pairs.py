29b258d7d34994407881f0a4c1a54d561c0c5886
Shinhyuk Park <141878853+shinPark43@users.noreply.github.com>
2025-06-14 00:16:33 -0500

    Update Find_Maximum_Number_of_String_Pairs.py

--- Solutions/Hash_Table/Find_Maximum_Number_of_String_Pairs.py
+++ Solutions/Hash_Table/Find_Maximum_Number_of_String_Pairs.py
@@ -4,21 +4,12 @@ class Solution(object):
         :type words: List[str]
         :rtype: int
         """
-        total = 0
-        hash_map = {}
-        for word in words:
-            hash_map[word] = hash_map.get(word, 0) + 1
+        total_pair = 0
+        reversed_set = set()
 
         for word in words:
-            reversed_word = word[::-1]
-            if (
-                reversed_word in hash_map and
-                hash_map[word] > 0 and
-                hash_map[reversed_word] > 0 and
-                word != reversed_word
-            ):
-                total += 1
-                hash_map[word] -= 1
-                hash_map[reversed_word] -= 1
-
-        return total
+            if word in reversed_set:
+                total_pair += 1
+            else:
+                reversed_set.add(word[::-1])
+        return total_pair
