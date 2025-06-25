7e57b70b000bdca9638580d2fa26f672246b08be
Shinhyuk Park <141878853+shinPark43@users.noreply.github.com>
2025-06-25 00:07:38 -0500

    Solved one sorting problem

--- Solutions/Sorting/Minimum_Average_of_Smallest_and_Largest_Element.py
+++ Solutions/Sorting/Minimum_Average_of_Smallest_and_Largest_Element.py
@@ -7,7 +7,7 @@ class Solution(object):
         nums.sort()
         min_avg = 51
         for i in range(len(nums)//2):
-            result = (nums[i] + nums[len(nums) - 1 - i]) / 2
+            result = (nums[i] + nums[len(nums) - 1 - i]) / 2.0
             if result < min_avg:
                 min_avg = result
         return min_avg
