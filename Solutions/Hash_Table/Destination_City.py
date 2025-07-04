fe07eebe405deed4479529b0e727f2bd3743aa09
Shinhyuk Park <141878853+shinPark43@users.noreply.github.com>
2025-07-04 00:15:22 -0500

    Updated Destination_City problem

--- Solutions/Hash_Table/Destination_City.py
+++ Solutions/Hash_Table/Destination_City.py
@@ -4,9 +4,16 @@ class Solution(object):
         :type paths: List[List[str]]
         :rtype: str
         """
-        start_city = set()
-        destination = set()
+        # start_city = set()
+        # destination = set()
+        # for path in paths:
+        #     start_city.add(path[0])
+        #     destination.add(path[1])
+        # return (destination - start_city).pop()
+        path_table = {}
         for path in paths:
-            start_city.add(path[0])
-            destination.add(path[1])
-        return (destination - start_city).pop()
+            path_table[path[0]] = path[1]
+        destination = paths[0][1]
+        while destination in path_table:
+            destination = path_table[destination]
+        return destination
